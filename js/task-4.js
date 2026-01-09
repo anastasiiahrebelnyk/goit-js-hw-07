f47fb15266a0f773adbae7b6220487c50db8cbdf
anastasiiahrebelnyk <anastasiiahrebelnyk@gmail.com>
2026-01-09 10:46:52 +0200

    do 4 task

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,35 @@
 
+const formEl = document.querySelector(".login-form");
+// formEl.setAttribute("name", "login-form");
+
+const handleFormSubmit = (event) => {
+
+    event.preventDefault();
+
+    const formEl = event.target;
+        const formData = {
+            email: formEl.elements.email.value,
+            password: formEl.elements.password.value
+        };
+    if (formData.email === "" || formData.password === "") {
+        alert('All form fields must be filled in');
+        return
+    }
+
+    else if (formData.email.includes("@") === false) {
+        alert('incorrect email');
+        return
+    }
+    else {
+        console.log(formData);
+    
+        formEl.reset()
+    };
+};
+    
+formEl.addEventListener("submit", handleFormSubmit);
+
+
+
+
+
