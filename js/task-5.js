9bec9c54c4c97ed939d1916ba5da1c1b17f4285b
anastasiiahrebelnyk <anastasiiahrebelnyk@gmail.com>
2026-01-09 11:00:45 +0200

    do 5 task

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function handleClick(event) {
+  colorEL.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = colorEL.textContent;
+}
+
+const changeButtonEl = document.querySelector(".change-color");
+changeButtonEl.addEventListener("click", handleClick);
+const colorEL = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
