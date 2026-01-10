ad2125681015b2cc9b11dc0cda3c0c74e39c3f73
anastasiiahrebelnyk <anastasiiahrebelnyk@gmail.com>
2026-01-10 13:45:57 +0200

    do 1 task

--- js/task-1.js
+++ js/task-1.js
@@ -1,42 +1,12 @@
 const categoriesQuantity = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoriesQuantity.length}`);
 
+categoriesQuantity.forEach(category => {
+    const titles = category.querySelector('h2');
+    const itemTitle = titles.textContent;
+    console.log(`Category: ${itemTitle}`);
 
-const item = [...document.querySelectorAll('.item h2')];
-// console.log(item);
-
-const liRefs = document.querySelectorAll('.item li');
-console.log(`Elements: ${liRefs.length}`);
-
-
-
-for (const el of item) {
-console.log(`Category: ${el.textContent}`);
-const liRefs = document.querySelectorAll('.item li');
-    console.log(`Elements: ${liRefs.length}`);
-
-}
-
-
-// for (const ref of liRefs) {
-//     console.log(`Elements: ${ref.length}`);
-
-// }
-
-// const liRefs = [...document.querySelectorAll('.item li')];
-// console.log(liRefs.length);
-
-// for (const el of liRefs) {
-// console.log(`Elements: ${el.textContent}`);
-
-// }
-
-
-
-
-// const refs = {
-// const categoriesQuantity = document.querySelectorAll('.item'),
-
-// }
-
-
+    const elements = category.querySelectorAll('ul li');
+    const elementsCount = elements.length;
+    console.log(`Elements: ${elementsCount}`);
+});
